a966a80a992dd53b4d5d9792981d7f080605ed63
Victor Chang <thevictorchang@gmail.com>
2015-11-23 16:29:30 +1300

    Added comment

---
+++ ContosoUniversity/DAL/SchoolContext.cs
@@ -0,0 +1,25 @@
+﻿using ContosoUniversity.Models;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using MySql.Data.Entity;
+
+namespace ContosoUniversity.DAL
+{
+    [DbConfigurationType(typeof(MySqlEFConfiguration))]
+    public class SchoolContext : DbContext
+    {
+
+        public SchoolContext() : base("SchoolContext")
+        {
+        }
+
+        public DbSet<Student> Students { get; set; }
+        public DbSet<Enrollment> Enrollments { get; set; }
+        public DbSet<Course> Courses { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+        }
+    }
+}
---
+++ ContosoUniversity/Models/CourseItem.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ContosoUniversity.Models
+{
+    //COMMENTSjnjnnnjnjn
+    //SOMETHING
+    public class CourseItem
+    {
+        public int ID { get; set; }
+        public int ItemsID { get; set; }
+    }
+}
